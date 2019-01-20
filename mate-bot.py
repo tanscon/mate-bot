eca5edd5dda2bafd2a65bef810ae34f8c7c332f8
Normen <normen.krug@web.de>
2019-01-20 23:49:36 +0100

    Formatted the source code

--- mate-bot.py
+++ mate-bot.py
@@ -9,23 +9,20 @@ logging.basicConfig(format='%(asctime)s - %(name)s -%(levelname)s - %(message)s'
 updater = Updater(token='732629412:AAGozAsOtKqhdK5nTs7_9GKv6rQ-0L754os')
 from telegram.ext import CommandHandler
 
-
 count = {"total": 0}
-path = sys.path[0]
 dispatcher = updater.dispatcher
 
 def start(bot, update):     
         try:
-            loadedCount = open("data.json","r")
+            with open('data.json','r') as fp:
+                loadedCount = json.load(fp)
             global count
-            count = json.load(loadedCount)
+            count = loadedCount
             print count
         except IOError:
             print "json.data wasnt found"
-        bot.send_message(chat_id=update.message.chat_id, text="I am a bot, please talk to me!")
+        bot.send_message(chat_id=update.message.chat_id, text="I will now start to count")
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
 
 def shutdown():
 	updater.stop()
@@ -38,22 +35,36 @@ def stop(bot, update):
             json.dump(count, outfile)
         threading.Thread(target=shutdown).start()
 
-stop_handler = CommandHandler('stop', stop)
-dispatcher.add_handler(stop_handler)
-
-
+def printOutMate(bot, update):
+        user = update.message.from_user
+        if str(user.id) in count:
+            content = user.first_name + " got " + str(count[str(user.id)]) + " mate already."
+            bot.send_message(chat_id=update.message.chat_id, text=content)
+        else:
+            bot.send_message(chat_id=update.message.chat_id, text="Drink more mate!")
 
 def add(bot, update, args):
+        print count
         user = update.message.from_user
-        if user.id in count:
-            count[user.id] = int(count[user.id]) + int(args[0])
+        userId = str(user.id)
+        if userId in count:
+            print "old"
+            count[userId] = int(count[userId]) + int(args[0])
         else:
-            count[user.id] = args[0]
+            print "new"
+            count[userId] = args[0]
         count["total"] = int(count["total"]) + int(args[0])
         bot.send_message(chat_id=update.message.chat_id, text=("added " + args[0]) + " Mate to " + user.first_name)
 
-
+#Add all handler to the dispatcher 
+print_handler = CommandHandler("mate", printOutMate)
+start_handler = CommandHandler('start', start)
+stop_handler = CommandHandler('stop', stop)
 add_handler = CommandHandler('add', add, pass_args=True)
+
+dispatcher.add_handler(print_handler)
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(stop_handler)
 dispatcher.add_handler(add_handler)
 
 updater.start_polling()
